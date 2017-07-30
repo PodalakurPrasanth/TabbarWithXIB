c682dffad0031b384c4b106eced1490d29a5b3f0
vinathy <vinathy.gunda@hedgehoglab.com>
2017-07-31 00:59:59 +0530

    minor changes

--- TabBar/TabBar/AppDelegate.swift
+++ TabBar/TabBar/AppDelegate.swift
@@ -24,7 +24,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             self.window?.rootViewController  = tabVC
      
         }else{
-    
             let loginVC:LoginVC = LoginVC(nibName: "LoginVC", bundle: nil)
             self.window?.rootViewController  = loginVC
             self.navigationVC?.navigationBar.isTranslucent = false
